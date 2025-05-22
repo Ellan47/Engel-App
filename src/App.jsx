d1bea4e7c627e2b368ee1c214833097b7955e0e5
Ellan47 <94044035+Ellan47@users.noreply.github.com>
2025-05-22 16:07:36 +0200

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+
+import React from "react";
+
+function App() {
+  return (
+    <div style={{ fontFamily: 'Arial', padding: '2rem' }}>
+      <h1>MatchWork</h1>
+      <p>Finde dein berufliches Perfect Match.</p>
+    </div>
+  );
+}
+
+export default App;
